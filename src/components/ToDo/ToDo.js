4616248eb3884dc01d414971f5facb25fbe8a5e8
Bharath Reddy Y A - <bharathreddyya@dxc.com>
2021-07-09 19:15:40 +0530

    latest

--- src/components/ToDo/ToDo.js
+++ src/components/ToDo/ToDo.js
@@ -2,10 +2,14 @@ import React, { useState, useCallback, useContext } from "react";
 import classes from "./ToDo.module.css";
 import Button from "../../UI/button/Button";
 import ToDoList from "../ToDoList/ToDoList";
+import Modal from "../../UI/Modal/Modal";
+
+let notification;
 
 const ToDo = (props) => {
   const [inputValue, setInputValue] = useState("");
   const [toDoData, setToDoData] = useState([]);
+  const [showModal, setShowModal] = useState(false);
 
   const buttonHandler = () => {
     if (inputValue.length !== 0) {
@@ -34,18 +38,51 @@ const ToDo = (props) => {
         const updatedData = { ...currentData[0], isCompleted: true };
         mainData[index] = { ...updatedData };
         setToDoData(mainData);
+      } else {
+        openModal();
+        Notification.requestPermission();
       }
     },
     [setToDoData, toDoData]
   );
 
+  const openModal = () => {
+    setShowModal(true);
+    var options = {
+      body: "you have already completed this task",
+      icon: "./assets/todo.jpg",
+      dir: "ltr",
+    };
+
+    notification = new Notification("TODO Notification!", options);
+  };
+
+  navigator.serviceWorker.register(`${process.env.PUBLIC_URL}/sw.js`);
+  Notification.requestPermission(function (result) {
+    if (result === "granted") {
+      navigator.serviceWorker.ready.then(function (registration) {
+        registration.showNotification("Notification with ServiceWorker");
+      });
+    }
+  });
+
+  const closeModal = () => {
+    setShowModal(false);
+    notification.close();
+  };
+
   return (
     <React.Fragment>
+      {showModal ? (
+        <Modal show={showModal} closeModal={closeModal}>
+          You have already completed this to-do!
+        </Modal>
+      ) : null}
       <div className={classes.todo}>
         <h1>TODO APP</h1>
-
         <input
           value={inputValue}
+          placeholder={"Please enter your to-do"}
           onChange={(event) => setInputValue(event.target.value)}
         ></input>
 
